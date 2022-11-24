4a355531c4c525a3ece2513685f8ba03102c1478
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-11-24 09:09:50 -0400

    implementar si registro el texto -Hola mundo- deberia poder ver la publicacion en la lista de publicaciones

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,9 +1,9 @@
-describe("Sumador", () => {
-  it("Shows the amount of the addition to the user", () => {
+describe("manejador de tweets", () => {
+  it("si registro el texto -Hola mundo- deberia poder ver la publicacion en la lista de publicaciones", () => {
     cy.visit("/");
-    cy.get("#primer-numero").type(4);
-    cy.get("#segundo-numero").type(5);
-    cy.get("#sumar-button").click();
-    cy.get("#resultado-div").should("contain", "9");
+    cy.get("#texto-tweet").type("Hola Mundo");
+    //cy.get("#segundo-numero").type(5);
+    cy.get("#tweet-button").click();
+    cy.get("#resultado-div").should("contain", "Hola Mundo");
   });
 });
